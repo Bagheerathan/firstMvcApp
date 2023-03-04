0deff0305ee9205de8cd42275bc324c01b7ea757
Bagheerathan <70306720+Bagheerathan@users.noreply.github.com>
2023-03-04 09:52:17 +0530

    Add project files.

---
+++ Controllers/HelloworldController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApp.Models;
+
+namespace MvcApp.Controllers
+{
+    public class HelloworldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel doggo = new DogViewModel()
+            { Name =" tommy", 
+              Age = 2
+            };
+            return View(doggo);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            //return View("index");
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public String Hello()
+        {
+            return "Hello this is hello function";
+        }
+    }
+}
+
+
